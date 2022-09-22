f8a87370d731606202543e54518c0c8cec4f032f
Pratima Nakhava <pnakhava@gmail.com>
2022-09-22 13:10:31 +0100

    First basic draft for todo app with navbar.

---
+++ todo-app/src/App.js
@@ -0,0 +1,11 @@
+import ButtonAppBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar />
+    </div>
+  );
+}
+
+export default App;
